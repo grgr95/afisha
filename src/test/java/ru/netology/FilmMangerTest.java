12ddbe3bcc89a76eeabab2cb562565d3e571f499
grgr_smrkv <grgrsmrkv@gmail.com>
2023-05-05 12:49:57 +0300

    mod FilmManagerTest.java

--- src/test/java/ru/netology/FilmMangerTest.java
+++ src/test/java/ru/netology/FilmMangerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class FilmManagerTest {
+public class FilmMangerTest {
     FilmManager manager = new FilmManager(5);
 
     @BeforeEach
